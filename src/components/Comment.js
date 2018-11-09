aaf455e5d17a0546da05d404a8376b0d4022aacc
Viacheslav Ptsarev <leemour@gmail.com>
2018-11-09 10:42:19 +0300

    Add selector to Comment with memoization

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {commentSelectorFactory} from '../selectors'
 
 function Comment({comment}) {
   return (
@@ -11,9 +12,17 @@ function Comment({comment}) {
 }
 
 Comment.propTypes = {
-  id: PropTypes.string.isRequired
+  comment: PropTypes.shape({
+    text: PropTypes.string,
+    user: PropTypes.string.isRequired
+  }).isRequired
 }
 
-export default connect((state, ownProps) => {
-  return {comment: state.comments.find((comment) => comment.id === ownProps.id)}
-})(Comment)
+const mapStateToProps = () => {
+  const commentSelector = commentSelectorFactory()
+  return (state, ownProps) => {
+    return { comment: commentSelector(state, ownProps) }
+  }
+}
+
+export default connect(mapStateToProps)(Comment)
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -2,9 +2,12 @@ import {createSelector} from 'reselect'
 
 const filtersGetter   = state => state.filters
 const arcitclesGetter = state => state.articles
+const commentsGetter  = state => state.comments
+const commentIdGetter = (state, props) => props.id
 
 export const filteredArticlesSelector = createSelector(
-  filtersGetter, arcitclesGetter, (filters, articles) => {
+  filtersGetter, arcitclesGetter,
+  (filters, articles) => {
     const {selected, dateRange: {from, to}} = filters
     // console.log('---', 'reselect articles')
 
@@ -14,5 +17,13 @@ export const filteredArticlesSelector = createSelector(
       if ((from && published < from) || (to && published > to)) return false
       return true
     })
+  }
+)
+
+export const commentSelectorFactory = () => createSelector(
+  commentsGetter, commentIdGetter,
+  (comments, id) => {
+    console.log('---', 'reselect comment')
+    return comments.find((comment) => comment.id === id)
   }
 )
